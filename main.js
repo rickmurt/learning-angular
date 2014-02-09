f105f59578a48abae43c2c02e10fdc08fd3ce281
Rick Murt <rickmurt@gmail.com>
2014-02-09 14:05:23 -0500

    Completed Part 2: Taking It for a spin -> AngularJS - Sharing Data Between Controllers

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function FirstCtrl($scope) {
-	
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', 
+	function() {
+		return {
+			message: "I'm data from a service"
+		};
+	}
+);
+
+function FirstCtrl($scope, Data) {
+	$scope.data = Data;
 }
 
-function SecondCtrl($scope) {
-	
+function SecondCtrl($scope, Data) {
+	$scope.data = Data;
 }
