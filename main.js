47f4e034d8e9a0d319457b71459794ce42babf3c
Rick Murt <rickmurt@gmail.com>
2014-02-09 20:05:47 -0500

    Completed Part 4: Directives -> AngularJS - Useful Behaviors

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var app = angular.module("behaviorApp", []);
 
 app.directive("enter",
 	function() {
-		return function(scope, element) {
+		return function(scope, element, attrs) {
 			element.bind("mouseenter",
 				function() {
-					console.log("I'm inside of you!");
+					element.addClass(attrs.enter);
 				}
 			);
 		}
@@ -14,10 +14,10 @@ app.directive("enter",
 
 app.directive("leave",
 	function() {
-		return function(scope, element) {
+		return function(scope, element, attrs) {
 			element.bind("mouseleave",
 				function() {
-					console.log("I'm leaving on a jet plane!");
+					element.removeClass(attrs.enter);
 				}
 			);
 		}
