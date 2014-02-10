163032acc8e22941f8d6dd576512c67287bcbe5d
Rick Murt <rickmurt@gmail.com>
2014-02-09 19:57:50 -0500

    Completed Part 4: Directives -> AngularJS - Basic Behaviors

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-var app = angular.module("superhero", []);
+var app = angular.module("behaviorApp", []);
 
-app.directive("superman", 
+app.directive("enter",
 	function() {
-		return {
-			restrict: "A",
-			link: function() {
-				alert("I'm working stronger");
-			}
+		return function(scope, element) {
+			element.bind("mouseenter",
+				function() {
+					console.log("I'm inside of you!");
+				}
+			);
 		}
 	}
 );
 
-app.directive("flash", 
+app.directive("leave",
 	function() {
-		return {
-			restrict: "A",
-			link: function() {
-				alert("I'm working faster");
-			}
+		return function(scope, element) {
+			element.bind("mouseleave",
+				function() {
+					console.log("I'm leaving on a jet plane!");
+				}
+			);
 		}
 	}
 );
