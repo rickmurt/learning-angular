75219703deeb2832659dce6c3e53968c3205308a
Rick Murt <rickmurt@gmail.com>
2014-02-15 13:47:26 -0500

    Completed Part 5: Scope -> AngularJS - Understanding Isolate Scope

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
-var app = angular.module("app",[]);
+var app = angular.module('choreApp', []);
  
-app.directive("country", function () {
-  return {
-    restrict: "E",
-    controller: function () {
-      this.makeAnnouncement = function (message) { 
-        console.log("Country says: " + message);
-      };
-    }
+app.controller("ChoreCtrl", function($scope){
+  $scope.logChore = function(chore){
+    alert(chore + " is done!");
   };
 });
-app.directive("state", function () {
+ 
+app.directive("kid", function() {
   return {
     restrict: "E",
-    controller: function () {
-      this.makeLaw = function (law) {
-        console.log("Law: " + law);
-      };
-    }
-  };
-});
-app.directive("city", function () {
-  return {
-    restrict: "E",
-    require: ["^country", "^state"],
-    link: function (scope, element, attrs, ctrls) {
-      ctrls[0].makeAnnouncement("This city rocks");
-      ctrls[1].makeLaw("Jump higher");
-    }
+    scope: {
+        done: "&"
+      },
+    template: '<input type="text" ng-model="chore">' +
+      '{{chore}}' +
+      '<div class="button" ng-click="done({chore: chore})">I\'m done</div>'
   };
 });
