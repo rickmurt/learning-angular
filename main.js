b8252bbae0314f6f3a4759cb92cef985b13e9df1
Rick Murt <rickmurt@gmail.com>
2014-02-15 15:05:36 -0500

    Completed Part 6: Application Structure and Organization -> An Alternative Approach to Controllers (I kind of like this way..)

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
 var app = angular.module('phoneApp', []);
  
-app.controller('AppController', function($scope){
-  $scope.leaveVoicemail = function(number, message){
-    alert('Number: ' + number + ' said: ' + message);
+app.controller("AppCtrl", function ($scope) {
+  this.sayHi = function () {
+    alert("hi");
   };
-});
  
-app.directive('phone', function(){
-  return {
-    restrict: 'E',
-    scope: {
-      number: '@',
-      network: '=',
-      makeCall: '&'
-    },
-    templateUrl: 'phone.html',
-    link: function(scope){
-        scope.networks = ["Verizon", "AT&T", "Sprint"];
-        scope.network = scope.networks[0];
-      }
-  };
+  return $scope.AppCtrl = this;
 });
