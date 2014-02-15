bc419fcb7755cd632bc5cede398d8385c52566fc
Rick Murt <rickmurt@gmail.com>
2014-02-15 15:52:25 -0500

    Completed Part 7: The View and the DOM -> AngularJS - angular.element

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var app = angular.module("app", [])
  
-app.controller("RoomCtrl", function(){
-  this.openDoor= function(){
-    alert("creak");
+app.directive("dumbPassword", function () {
+  var validElement = angular.element('<div>{{ model.input }}</div>');
+ 
+  var link = function (scope) {
+    scope.$watch("model.input", function (value) {
+      if(value === "password") {
+        validElement.toggleClass("alert-box alert");
+      }
+    });
   };
  
-  this.buttonTitle = "I'm a button";
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "dumbpass.html",
+    compile: function (tElem) {
+      tElem.append(validElement);
+ 
+      return link;
+    }
+  }
 });
