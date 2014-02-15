c3b3989cecdfb6e8ad1a609257a16590933386da
Rick Murt <rickmurt@gmail.com>
2014-02-15 17:11:14 -0500

    Completed Part 9: Routing - AngularJS -  api

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var app = angular.module("app", ['ngRoute']);
 
-app.config(function($routeProvider) {
-	$routeProvider
-		.when('/',
-			{
-				templateUrl: "app.html",
-				controller: "AppCtrl"
-			}
-	)
-})
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/',
+    {
+      templateUrl: "app.html",
+      controller: "AppCtrl"
+    })
+    .when('/pizza',
+    {
+      template: "Yum!!"
+    })
+    .otherwise({
+      template: "This doesn't exist!"
+    })
+});
 
 app.controller("AppCtrl", function($scope, $route) {
 	$scope.model = {
