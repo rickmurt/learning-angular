7ecdb0f55083807f3f021b7c0d28caeafdac979c
Rick Murt <rickmurt@gmail.com>
2014-02-15 17:43:04 -0500

    Completed Part 9: Routing - AngularJS - Resolve

--- main.js
+++ main.js
@@ -5,29 +5,21 @@ app.config(function ($routeProvider) {
     .when('/',
     {
       templateUrl: "app.html",
-      controller: "AppCtrl"
+      controller: "AppCtrl",
+		resolve: {
+			app: function ($q, $timeout) {
+			  var defer = $q.defer();
+			  $timeout(function () {
+			    defer.resolve(); 
+			  }, 2000);
+			  return defer.promise;
+			}
+		}
     }
   )
 });
  
 app.controller("AppCtrl", function ($scope, $q) {
-	var defer = $q.defer();
- 
-	defer.promise
-	.then(function (weapon) {
-	  alert("You can have my " + weapon);
-	  return "bow";
-	})
-	.then(function (weapon) {
-	  alert("And my " + weapon);
-	  return "axe";
-	})
-	.then(function (weapon) {
-	  alert("And my " + weapon);
-	});
-
-	defer.resolve("sword");
-
 	$scope.model = {
 		message: "This is my app!!!"
 	}
