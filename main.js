2e176d8915929e5a2ddef141844d8db2b0a93533
Rick Murt <rickmurt@gmail.com>
2014-02-16 10:47:15 -0500

    Completed Part 10: Examples and Analysis - Building Zippy

--- main.js
+++ main.js
@@ -1,49 +1,21 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', []);
  
-app.config(function ($routeProvider, $locationProvider) {
-  $routeProvider
-    .when('/',
-    {
-      templateUrl: "app.html",
-      controller: "ViewCtrl"
-    })
-    .when('/new',
-    {
-      templateUrl: "new.html",
-      controller: "NewCtrl",
-      resolve: {
-        loadData: viewCtrl.loadData
-      }
+app.directive("zippy", function(){
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: {
+      title: "@"
+    },
+    template: '<div>' +
+      '<h3 ng-click="toggleContent()">{{title}}</h3>' +
+      '<div ng-show="isContentVisible" ng-transclude></div>' +
+      '</div>',
+    link: function(scope){
+      scope.isContentVisible = false;
+      scope.toggleContent = function(){
+        scope.isContentVisible = !scope.isContentVisible;
+      };
     }
-  )
+  };
 });
- 
-app.controller("AppCtrl", function ($rootScope, $scope, $route, $location) {
-  $rootScope.$on("$routeChangeStart", 
-                 function (event, current, previous, rejection) {
-    console.log($scope, $rootScope, $route, $location);
-  });      
-  $rootScope.$on("$routeChangeSuccess", 
-                 function (event, current, previous, rejection) {
-    console.log($scope, $rootScope, $route, $location);
-  });
-});
- 
-var viewCtrl = app.controller("ViewCtrl", function ($scope, $route, $location) {
-  $scope.changeRoute = function () {
-    console.log($scope);
-    $location.path("/new");
-  }
-});
- 
-app.controller("NewCtrl", function($scope, loadData, $template) {
-  console.log($scope, loadData, $template);
-});
- 
-viewCtrl.loadData = function ($q, $timeout) {
-  var defer = $q.defer();
-  $timeout(function () {
-    defer.resolve({message:"success"}); 
-  }, 2000);
-  return defer.promise;
-};
